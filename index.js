6d2a8253a381362f286413f5c0b0ea156544472b
sdelane2 <seanmartindelaney@gmail.com>
2020-11-16 22:44:19 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+let element = document.querySelector('h1#victory');
+element.innerHTML = "<h1>Sean is the champion</h1>"
+
